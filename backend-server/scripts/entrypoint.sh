ab073adfc3ba09509bbd2c4a74220e921ba3462d
Charliehyin <charliehyin@gmail.com>
2025-03-10 21:57:43 -0500

    Modifying backend to be production server

--- backend-server/scripts/entrypoint.sh
+++ backend-server/scripts/entrypoint.sh
@@ -2,4 +2,4 @@
 # Run the assistant initialization script before starting the server
 python scripts/init_assistant.py
 # Start the Flask application
-exec python app.py
+gunicorn --workers=4 --bind=0.0.0.0:5000 wsgi:app
