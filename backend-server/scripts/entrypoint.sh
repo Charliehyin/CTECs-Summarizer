3b6fdbbab36def2f62408031273b9ccc2abebeb0
EC2 Default User <ec2-user@ip-172-31-30-17.us-east-2.compute.internal>
2025-03-12 06:02:21 +0000

    Creating EC2 entrypoint file

--- backend-server/scripts/entrypoint.sh
+++ backend-server/scripts/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # Run the assistant initialization script before starting the server
-python scripts/init_assistant.py
-python scripts/compute_embeddings.py
+/home/ec2-user/CTECs-Summarizer/venv/bin/python3.12 /home/ec2-user/CTECs-Summarizer/backend-server/scripts/init_assistant.py
+# python scripts/compute_embeddings.py
 
 # Ensure the directory for the PID file exists
-mkdir -p /var/pids
+# mkdir -p /var/pids
 
 # Remove stale PID file, if any
-rm -f /var/pids/web.pid
+# rm -f /var/pids/web.pid
 
-# Start the Flask application with Gunicorn, writing the PID to /var/pids/web.pid
-gunicorn --workers=4 --bind=0.0.0.0:8000 --pid /var/pids/web.pid wsgi:app
+# Start the Flask application with Gunicorn
+cd /home/ec2-user/CTECs-Summarizer/backend-server
+/home/ec2-user/CTECs-Summarizer/venv/bin/gunicorn --workers=4 --bind=0.0.0.0:8000 wsgi:app
